d382e37d572a5bc511d8d8d80bfbccd5e97fd2a1
schlamar <marc.schlaich@gmail.com>
2014-03-19 19:35:57 +0100

    Complete rewrite: generate a table with a range of maximum points.

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import itertools
 import logging
 import os
 
-from flask import Flask, render_template, flash, request, jsonify
+from flask import Flask, render_template, flash, request
 
-from lib import calculate_data
+import lib
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler())
 app.secret_key = os.environ['SECRET_KEY']
-
-
-@app.route('/_points_six')
-def points_six_recomm():
-    p_max = request.args.get('points_max', 0, type=int)
-    if p_max in range(0, 13):
-        res = 0
-    elif p_max in range(13, 25):
-        res = 1
-    elif p_max in range(25, 37):
-        res = 2
-    elif p_max in range(37, 49):
-        res = 3
-    else:
-        res = 4
-
-    return jsonify(result=res)
 
 
 @app.route('/')
@@ -38,15 +22,25 @@ def form():
 @app.route('/result', methods=['POST'])
 def result():
     try:
-        points_max = int(request.form['points_max'])
-        points_six = int(request.form['points_six'])
+        points_start = int(request.form['points_start'])
+        points_end = int(request.form['points_end'])
+        if points_end < 0 or points_start < 0:
+            raise ValueError
+        if points_end < points_start:
+            raise ValueError
+        if points_end - points_start > 200:
+            raise ValueError
+        factor_six = float(request.form['factor_six'])
+        if not 0 < factor_six < 1:
+            raise ValueError
         steps = request.form['steps']
     except ValueError:
         flash(u'Keine gültigen Zahlen angeben.')
-        data = list()
+        table = list()
     else:
-        data = calculate_data(points_max, points_six, steps)
-    return render_template('result.html', data=data)
+        table = lib.get_table(points_start, points_end, factor_six, steps)
+        table = itertools.izip(*table)
+    return render_template('result.html', table=table)
 
 
 if __name__ == '__main__':
--- lib.py
+++ lib.py
@@ -4,35 +4,36 @@ import math
 
 
 STEP_FACTORS = {'quarter': 4, 'tenth': 10}
+STEP_INCREMENT = {'quarter': 0.25, 'tenth': 0.1}
 
 
 def frange(limit1, limit2, increment):
-    count = int(math.ceil(limit2 - limit1) / increment)
+    count = int(math.ceil((limit2 - limit1) / increment))
     return (limit1 + n * increment for n in xrange(count))
 
 
-def calc_grade(points_max, points_six, points, steps):
+def calc_points(points_max, points_six, grade_step, sum_steps):
+    dist_points = points_max - points_six
+    diff = (dist_points / float(sum_steps)) * grade_step
+    diff = math.ceil(diff * 2) / 2  # round to 0.5
+    return points_max - diff
+
+
+def calc_row(points_max, points_six, steps):
     step_factor = STEP_FACTORS[steps]
-    grade = 6 - (points - points_six) * (5.0 / (points_max - points_six))
-    grade = math.floor(grade * step_factor) / step_factor
-    if grade > 6:
-        return 6.
-    return grade
+    sum_steps = 5 * step_factor
+    for grade_step in xrange(sum_steps + 1):
+        yield calc_points(points_max, points_six, grade_step, sum_steps)
 
 
-def calculate_data(points_max, points_six, steps):
+def get_grades_row(steps):
+    inc = STEP_INCREMENT[steps]
+    return frange(1.0, 6.0 + inc, inc)
 
-    data = list()
-    grade = 6.
-    points_end = 0.
-    points_start = 0.
 
-    for points in frange(0, points_max + 1, 0.5):
-        new_grade = calc_grade(points_max, points_six, points, steps)
-        if new_grade != grade:
-            data.append((points_end, points_start, grade))
-            points_start = points
-            grade = new_grade
-        points_end = points
-
-    return reversed(data)
+def get_table(points_start, points_end, factor_six=0.082, steps='quarter'):
+    print points_start, points_end, factor_six, steps
+    yield get_grades_row(steps)
+    for points in xrange(points_start, points_end + 1):
+        points_six = int(points * factor_six)
+        yield calc_row(points, points_six, steps)
